e40c24e2d295263d0017b20352c4bef94a0c518e
yanuarfa <yanuarfatur41@gmail.com>
2022-12-17 21:27:43 +0700

    [ADD] Added update book handler

--- src/handler.js
+++ src/handler.js
@@ -172,4 +172,78 @@ const getDetailBookHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBookHandler, getDetailBookHandler };
+const updateBookHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const finished = pageCount === readPage;
+  const updatedAt = new Date().toISOString();
+
+  const bookIndex = books.findIndex((book) => book.id === bookId);
+
+  if (!name || name === ' ') {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (Number(readPage) > Number(pageCount)) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (bookIndex !== -1) {
+    books[bookIndex] = {
+      ...books[bookIndex],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBookHandler,
+  getDetailBookHandler,
+  updateBookHandler,
+};
